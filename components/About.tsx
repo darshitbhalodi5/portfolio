432d08d3fa9bbcbabf7a59643c78d80b5dcf9b93
darshitbhalodi5 <darshitbhalodi@gmail.com>
2025-04-19 15:40:40 +0530

    Enhancement: Updated Contact and Footer components

--- components/About.tsx
+++ components/About.tsx
@@ -19,7 +19,7 @@ const About = () => {
               {/* Profile image */}
               <div className="relative z-10 w-full h-full overflow-hidden rounded-lg">
                 <Image
-                  src="/profile.jpg" 
+                  src="/profile.png" 
                   alt="Darshit Bhalodi" 
                   fill
                   className="object-cover"
